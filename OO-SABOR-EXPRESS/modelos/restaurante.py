45ba8e0cdd42e0dc76fa7f176d5d18d15c6f42e1
AndreSouzaTI <andresouza_ti@yahoo.comgit config --global user.emailgit config user.emailgit config user.namegit config init.defaultBranchgit config --global user.email andresouza_ti@yahoo.com>
2024-07-30 17:39:58 -0300

    inclusão de cardápio e término do app de orientação e objeto

--- OO-SABOR-EXPRESS/modelos/restaurante.py
+++ OO-SABOR-EXPRESS/modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     """Representa um restaurante e suas características."""
@@ -17,6 +18,7 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
@@ -62,7 +64,20 @@ class Restaurante:
         return media
 
 
-
+    def adicionar_no_cardapio(self,item):
+        if isinstance(item,ItemCardapio):
+            self._cardapio.append(item)
+    
+    @property
+    def exibir_cardapio(self):
+        print(f'Cardapio do restaurante {self._nome}\n')
+        for i,item in enumerate(self._cardapio,start=1):
+            if hasattr(item,'descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
+                print(mensagem_bebida)
 
 
 
