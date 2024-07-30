a19651b48ea22c9675c92f2048b708297c6cf20c
AndreSouzaTI <andresouza_ti@yahoo.comgit config --global user.emailgit config user.emailgit config user.namegit config init.defaultBranchgit config --global user.email andresouza_ti@yahoo.com>
2024-07-30 09:27:08 -0300

    alter erro

--- OO-SABOR-EXPRESS/modelos/exercicios.py
+++ OO-SABOR-EXPRESS/modelos/exercicios.py
@@ -1,5 +1,3 @@
-# Respostas
-
 # 1) Implemente uma classe chamada Carro com os atributos básicos, como modelo, cor e ano. Crie uma instância dessa classe e atribua valores aos seus atributos.
 
 class Carro:
